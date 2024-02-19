c29db95117e8fe4386451b9334adef737a207e56
Hassuk1 <misha.carpushov1@gmail.com>
2024-02-19 18:13:11 +0300

    All done

--- src/exercise02/app/src/main/java/com/example/repeat_the_sequence/enums/AppScreens.kt
+++
@@ -1,5 +0,0 @@
-package com.example.repeat_the_sequence.enums
-
-enum class AppScreens(val route: String) {
-  MENU("MenuScreen"), GAME("GameScreen"), FREEGAME("FreeGameScreen"), LOSE("LoseScreen")
-}
